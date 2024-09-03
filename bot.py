7e95d953a4957a6ec97680d16bbc24b7075aaf53
fernandorsdiniz81 <137434584+fernandorsdiniz81@users.noreply.github.com>
2024-09-03 07:16:25 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,8 @@ class Bot:
         self.APP_SECRET = os.environ["APP_SECRET"]
         self.VERSION = os.environ["VERSION"]
         self.PHONE_NUMBER_ID = os.environ["PHONE_NUMBER_ID"]
-        self.COMMAND = os.environ["COMMAND"]
+        self.MY_NUMBER = os.environ["MY_NUMBER"]
+        self.ON_OFF_COMMAND = os.environ["ON_OFF_COMMAND"]
         self.bot_status = True
         self.bot_intelligence = AIEngine()
         self.virtual_assistant = VirtualAssistant()
@@ -70,42 +71,41 @@ class Bot:
         return response.status_code
 
 
+    def login(self, wa_id, text): # comando para ligar/desligar o bot
+        if wa_id == self.MY_NUMBER and text == self.ON_OFF_COMMAND.lower():
+            if self.bot_status == True:
+                self.bot_status = False
+            else:
+                self.bot_status = True
+            self.send_message(wa_id, "😴" if self.bot_status == False else "🤓")
+            print(f"status do bot: {self.bot_status}")
+        
+
+
     def read_message(self, response: dict) -> tuple:
         name = response["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"]
         wa_id = response["entry"][0]["changes"][0]["value"]["contacts"][0]["wa_id"]
         timestamp = response["entry"][0]["changes"][0]["value"]["messages"][0]["timestamp"]
         text = response["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"]
+        self.log_recorder(name, wa_id, timestamp, text)
         print(f"\nmensagem de {name} ({wa_id}, {timestamp}):\n{text}\n")
         return name, wa_id, timestamp, text
 
-
-    def login(self, text): # Serve para ligar/desligar o bot
-        if text == self.COMMAND.lower():
-            if self.bot_status == True:
-                self.bot_status = False
-            else:
-                self.bot_status = True
-        print(self.bot_status)
-
      
     def answer_message(self, name: str, wa_id: str, timestamp: str, text: str) -> None:
-        self.login(text)
-        if self.bot_status == True and text != self.COMMAND:
+        self.login(wa_id, text) # sempre é verificado se houve comando para alterar o status do bot
+        if self.bot_status == True and text != self.ON_OFF_COMMAND: # sempre é verificado se houve comando para alterar o status do bot
             answer = self.virtual_assistant.user_question(name, wa_id, text)
             response = self.send_message(wa_id, answer)
             if response == 200:
                 print(f"mensagem do bot ({timestamp}):\n{answer}\n")
+                self.log_recorder("eu", "bot", timestamp, answer)
             else:
                 print(f"Falha no envio da mensagem: {response}")
     
 
-    def log_record():
-        pass
-
-
-# Enviando mensagem template para meus números:        
-# bot = Bot()
-# recipients = ["5531987772280", "5531920016652"]
-# for recipient in recipients:
-#     # bot.send_template_message(recipient)
+    def log_recorder(self, name: str, wa_id: str, timestamp: str, text: str) -> None:
+        with open("log.txt", "a+") as file:
+            dialog = f"{name} ({wa_id})\n{timestamp}\n{text}\n\n"
+            file.write(dialog)
 
